9252d7e51adea64760bae7b83ac9f933c6a83dd6
MSV1990 <htconemailbox@gmail.com>
2019-04-30 11:34:07 +0300

    feat: add localstorage function for figures and button to reset local storage

--- pallete/index.js
+++ pallete/index.js
@@ -10,7 +10,9 @@ prevColor = document.getElementById('prevCol').style.backgroundColor,
 curColor = document.getElementById('curCol').style.backgroundColor,
 theInput = document.getElementById("color");
 
-
+function clearLocal(){
+    localStorage.clear();
+}
 //select tool with shortcut
 document.onkeyup = function(e) {
     if (e.ctrlKey && e.altKey && e.keyCode == 80) {
@@ -123,12 +125,18 @@ document.addEventListener('click', (event)=> {
    function justDoIt(){
         if(currentTool == 'bucket'){
             event.target.style.backgroundColor = curColor;
+            let divId =  event.target.id;
+            localStorage.setItem(divId,curColor );
         }
         if(currentTool == 'transform' &&  event.target.style.borderRadius == '130px'){
             event.target.style.borderRadius = '0';
+            let divR =  event.target.id+'r';
+            localStorage.setItem(divR,'o' );
         }
         else if(currentTool == 'transform'){
             event.target.style.borderRadius = '130px'; 
+            let divR =  event.target.id+'r';
+            localStorage.setItem(divR,'130px' );
         }
         if(currentTool == 'move'){
          
@@ -163,10 +171,85 @@ img9.addEventListener('click', (event)=>{
     justDoIt()
 })
 window.onload = function(){
+    //saved tool 
     if(localStorage.getItem('currentTool') !== null){
         let tool = localStorage.getItem('currentTool');
         let color = localStorage.getItem('currentToolcolor');
         currentTool = tool;
         document.getElementById(tool).style.backgroundColor = color;
     }
+    //saved bgcolor
+    if(localStorage.getItem('img1') !== null){
+       let img1color =  localStorage.getItem('img1');
+       img1.style.backgroundColor = img1color;
+    }
+    if(localStorage.getItem('img2') !== null){
+        let img2color =  localStorage.getItem('img2');
+        img2.style.backgroundColor = img2color;
 }
+if(localStorage.getItem('img3') !== null){
+    let img3color =  localStorage.getItem('img3');
+    img3.style.backgroundColor = img3color;
+}
+if(localStorage.getItem('img4') !== null){
+    let img4color =  localStorage.getItem('img4');
+    img4.style.backgroundColor = img4color;
+}
+if(localStorage.getItem('img5') !== null){
+    let img5color =  localStorage.getItem('img5');
+    img5.style.backgroundColor = img5color;
+}
+    if(localStorage.getItem('img6') !== null){
+        let img6color =  localStorage.getItem('img6');
+        img6.style.backgroundColor = img6color;
+    }
+        if(localStorage.getItem('img7') !== null){
+            let img7color =  localStorage.getItem('img7');
+            img7.style.backgroundColor = img7color;
+        }
+            if(localStorage.getItem('img8') !== null){
+                let img8color =  localStorage.getItem('img8');
+                img8.style.backgroundColor = img8color;
+            }
+                if(localStorage.getItem('img9') !== null){
+                    let img9color =  localStorage.getItem('img9');
+                    img9.style.backgroundColor = img9color;
+                }
+                //saved transform
+                if(localStorage.getItem('img1r') !== null){
+                    let img1r =  localStorage.getItem('img1r');
+                    img1.style.borderRadius = img1r;
+                 }
+                 if(localStorage.getItem('img2r') !== null){
+                     let img2r =  localStorage.getItem('img2r');
+                     img2.style.borderRadius = img2r;
+             }
+             if(localStorage.getItem('img3r') !== null){
+                 let img3r =  localStorage.getItem('img3r');
+                 img3.style.borderRadius = img3r;
+             }
+             if(localStorage.getItem('img4r') !== null){
+                 let img4r =  localStorage.getItem('img4r');
+                 img4.style.borderRadius = img4r;
+             }
+             if(localStorage.getItem('img5r') !== null){
+                 let img5r =  localStorage.getItem('img5r');
+                 img5.style.borderRadius = img5r;
+             }
+                 if(localStorage.getItem('img6r') !== null){
+                     let img6r =  localStorage.getItem('img6r');
+                     img6.style.borderRadius = img6r;
+                 }
+                     if(localStorage.getItem('img7r') !== null){
+                         let img7r =  localStorage.getItem('img7r');
+                         img7.style.borderRadius = img7r;
+                     }
+                         if(localStorage.getItem('img8r') !== null){
+                             let img8r =  localStorage.getItem('img8r');
+                             img8.style.borderRadius = img8r;
+                         }
+                             if(localStorage.getItem('img9r') !== null){
+                                 let img9r =  localStorage.getItem('img9r');
+                                 img9.style.borderRadius = img9r;
+                             }
+            }
